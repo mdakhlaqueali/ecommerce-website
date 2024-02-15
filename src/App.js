d73f343187f9201460ba75b9cd58033cb35a42f1
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-02-15 23:43:13 +0530

    Adding Contact US Page via REACT ROUTER

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Showproducts from './components/Showproduct.jsx';
 import MyNavbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
+import Contact from './pages/Contact';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Showproducts />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </div>
   );
