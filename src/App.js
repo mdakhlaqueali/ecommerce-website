3147f60b7f6d0de01318b2d542a6c96927ae01c5
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-02-15 22:59:58 +0530

    Cart Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Showproducts from './components/Showproducts.jsx';
+import MyNavbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <MyNavbar/>
       <Showproducts/>
     </div>
   );
